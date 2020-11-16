3f6c74c97fb9014c0150e9537b5d0f3f5b14fbe6
Gary Zhexi Zhang <gary.zhexi.zhang@gmail.com>
2020-11-16 15:49:05 +0000

    Add files via upload
    embarassing add

---
+++ assets/js/scatter.js
@@ -0,0 +1,127 @@
+// set the dimensions and margins of the graph
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
+    width = 14000 - margin.left - margin.right,
+    height = window.innerHeight;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("../assets/json/frequency50.csv", function(data) {
+
+  // List of groups = header of the csv files
+  var keys = data.columns.slice(1)
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain(d3.extent(data, function(d) { return d.year; }))
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSize(-height).tickValues([1950, 1960, 1970, 1980, 1990, 2000]))
+    .select(".domain").remove()
+  // Customization
+  svg.selectAll(".tick line").attr("stroke", "#b8b8b8")
+
+  // Add X axis label:
+  svg.append("text")
+      .attr("text-anchor", "end")
+      .attr("x", width)
+      .attr("y", height-30 )
+      .text("Time (year)");
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([-30000, 30000])
+    .range([ height, 0 ]);
+
+  // color palette
+  var color = d3.scaleOrdinal()
+    .domain(keys)
+    .range(d3.schemeSet3);
+
+  //stack the data?
+  var stackedData = d3.stack()
+    .offset(d3.stackOffsetSilhouette)
+    .keys(keys)
+    (data)
+
+  // create a tooltip
+  var Tooltip = d3.select("body").append("div") 
+    .append("text")
+    .style("opacity", 0)
+    .attr("class", "tooltip")       
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    Tooltip.style("opacity", 1)
+    d3.selectAll(".myArea").style("opacity", .2)
+    d3.select(this)
+      // .style("stroke", "black")
+      .style("opacity", 1)
+  }
+  var mousemove = function(d,i) {
+    grp = keys[i]
+    Tooltip.text(grp)
+  }
+  var mouseleave = function(d) {
+    Tooltip.style("opacity", 0)
+    d3.selectAll(".myArea").style("opacity", 1).style("stroke", "none")
+   }
+
+  // Area generator
+  var area = d3.area()
+    .x(function(d) { return x(d.data.year); })
+    .y0(function(d) { return y(d[0]); })
+    .y1(function(d) { return y(d[1]); })
+
+  // Show the areas
+  svg
+    .selectAll("mylayers")
+    .data(stackedData)
+    .enter()
+    .append("path")
+      .attr("class", "myArea")
+      .style("fill", function(d) { return color(d.key); })
+      .attr("d", area)
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave)
+
+})
+
+
+
+// d3.json('../assets/json/frequency.json', function(data) {
+
+
+
+
+//   var years = Object.keys(data);
+//   var terms = Object.keys(data["1950"])
+//   const array = data
+//   var cleanData = []
+//   // console.log(array)
+
+//   for (var i = 0; i < years.length; i++) {
+//     for (var j = 0; j < Object.keys(data[years[i]]).length; j++) {
+//       cleanData.push({ x : years[i], y : data[years[i]]})
+//     }
+//   }
+
+//   // for (const [key, value] of Object.entries(array)) {
+//   //     cleanData.push( { x : key, y : value.weighting } );
+//   // }
+
+//   console.log(years)
+//   console.log(cleanData)
+  
+
+
+
+// })
